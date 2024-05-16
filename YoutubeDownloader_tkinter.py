41988b35e4780578263ec9ef4c9185f966f3b631
Aryan Desai <desai.aryan124@gmail.com>
2024-05-16 18:43:29 +0530

    Added Spaces so that it is easier to read the code

--- YoutubeDownloader_tkinter.py
+++ YoutubeDownloader_tkinter.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 from pytube import YouTube
+
 root=tk.Tk()
 root.title("python")
 root.geometry("400x300")
+
 link= tk.StringVar()
 lb1=tk.Label(root,text="url :")
 lb1.pack()
 entry=tk.Entry(root,width=50,textvariable=link)
 entry.pack()
+
 def python():
     a=YouTube(str(link.get()))
     video= a.streams.first()
